fb33deaae7bffb87a01fc94d8cd04f994b08ec50
Himanshu Mishra <himanshu2014iit@gmail.com>
2016-04-13 00:19:02 +0530

    Indent using spaces

--- src/parameter1.py
+++ src/parameter1.py
@@ -1,29 +1,29 @@
 from __future__ import division
 
 def distance_between_cars(speed, response_time=1):
-	'''Returns the average distance between 2 vehicles.
+    '''Returns the average distance between 2 vehicles.
 
-	Parameters
-	==========
+    Parameters
+    ==========
 
-	speed : Speed in km/hr units
-	response_time : Response time to be given in seconds.
-	    Assumed to be 1 second for our purpose.
+    speed : Speed in km/hr units
+    response_time : Response time to be given in seconds.
+        Assumed to be 1 second for our purpose.
 
-	Returns
-	=======
+    Returns
+    =======
 
-	min_distance : a float value in kms corresponding to the minimum safe distance between
-	two vehicles given a path with specified speed limit and a response time.
+    min_distance : a float value in kms corresponding to the minimum safe distance between
+    two vehicles given a path with specified speed limit and a response time.
 
-	'''
-	speed_units = (5 * speed) / 18
-	# the minimum safe distance needed to avoid any mishap
-	min_distance = speed_units * response_time  
+    '''
+    speed_units = (5 * speed) / 18
+    # the minimum safe distance needed to avoid any mishap
+    min_distance = speed_units * response_time
     return min_distance/1000
 
 def density_of_cars(route, cars=None):
-	'''Calculates the density of cars on a given route.
+    '''Calculates the density of cars on a given route.
 
     Parameters
     ==========
@@ -40,10 +40,10 @@ def density_of_cars(route, cars=None):
         is returned. In all other cases, the value returned is the number of cars
         per km running on the concerned path.
 
-	'''
-	path_length = route.length
-	speed = route.speed_limit
-	lanes = route.lanes
+    '''
+    path_length = route.length
+    speed = route.speed_limit
+    lanes = route.lanes
     # The avaerage length between 2 cars is assumed to be 4.5 m
     avg_length = 0.0045
     if cars is None:
@@ -52,34 +52,34 @@ def density_of_cars(route, cars=None):
         # Just to make the logic clearer
         car_density = ((path_length / total_length) * lanes)/ path_length
     else:
-    	car_density = cars / path_length
+        car_density = cars / path_length
     return car_density
 
 def _threshold_check(car_density, threshold):
-	'''Compares particular car density level with the threshold value.
+    '''Compares particular car density level with the threshold value.
 
-	Reeturns True if the number of cars on a paritcular road is more than
-	the threshold set for the particular road.
+    Reeturns True if the number of cars on a paritcular road is more than
+    the threshold set for the particular road.
 
-	'''
-	if threshold <= cars_density:
-		return True
-	else:
-		return False
+    '''
+    if threshold <= cars_density:
+        return True
+    else:
+        return False
 
 def check_parameter1(route):
-	'''Checks whether the car density on a path is more than the threshold
-	value or not. 
+    '''Checks whether the car density on a path is more than the threshold
+    value or not.
 
-	Parameter
-	=========
-	route : the concerned route
+    Parameter
+    =========
+    route : the concerned route
 
-	'''
-	car_den = route.car_density
-	threshold = route.threshold
-	parameter = route.parameter1
-	if _threshold_check(car_dens, threshold):
-		parameter = True
-	else:
-		parameter = False
+    '''
+    car_den = route.car_density
+    threshold = route.threshold
+    parameter = route.parameter1
+    if _threshold_check(car_dens, threshold):
+        parameter = True
+    else:
+        parameter = False
