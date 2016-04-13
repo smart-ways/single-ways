c48b67c536ec37dd8fa72b6136bd70e35bf8a1a3
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2016-04-13 23:06:21 +0530

    added fuzzy rules

--- src/fuzzy.py
+++ src/fuzzy.py
@@ -46,9 +46,9 @@ def density_of_cars(city, route, cars=None):
         per km running on the concerned path.
 
     """
-    path_length = G[route[0]][route[1]]["length"]
-    speed = G[route[0]][route[1]]["speed_limit"]
-    lanes = G[route[0]][route[1]]["lanes"]
+    path_length = city[route[0]][route[1]]["length"]
+    speed = city[route[0]][route[1]]["speed_limit"]
+    lanes = city[route[0]][route[1]]["lanes"]
     # The avaerage length between 2 cars is assumed to be 4.5 m
     avg_length = 0.0045
     if cars is None:
@@ -58,6 +58,7 @@ def density_of_cars(city, route, cars=None):
         cars = ((path_length / total_length) * lanes)
     car_density = cars / path_length
     return car_density
+
 
 def _threshold_check(car_density, threshold):
     """Compares particular car density level with the threshold value.
@@ -71,6 +72,7 @@ def _threshold_check(car_density, threshold):
     else:
         return False
 
+
 def check_parameter1(city, route):
     """Checks whether the car density on a path is more than the threshold
     value or not.
@@ -83,12 +85,60 @@ def check_parameter1(city, route):
         A tuple of indices of vertices which are connected by the route.
 
     """
-    route = city[route[0]][route[1]]
+    route_up = city[route[0]][route[1]]
+    route_down = city[route[1]][route[0]]
 
-    car_density = route["car_density"]
-    threshold = route["threshold"]
+    up_car_density = route_up["car_density"]
+    up_threshold = route_up["threshold"]
 
-    if _threshold_check(car_density, threshold):
-        route["parameter1"] = True
+    down_car_density = route_down["car_density"]
+    down_threshold = route_down["threshold"]
+
+    if _threshold_check(up_car_density, up_threshold) and not _threshold_check(down_car_density, down_threshold):
+        route_up["parameter1"] = True
+        route_down["parameter1"] = False
+
+    if not _threshold_check(up_car_density, up_threshold) and _threshold_check(down_car_density, down_threshold):
+        route_down["paramter1"] = True
+        route_up["parameter1"] = False
     else:
-        route["parameter1"] = False
+        route_up["parameter1"] = route_down["parameter1"] = False
+
+
+def fuzzify(a):
+    '''Returns the fuzzy membership value of a given number using the
+    membership functions as specified in the model.
+
+    Parameters
+    =========
+    a : float
+        A float value belonging to Interval[0, 1]
+
+    '''
+    if a>=0.9:
+       return 10*a - 9
+    else:
+        return 0
+
+
+def fuzzy_route(city, route):
+    '''Returns the fuzzy value of the traffic percentage in a given route.
+
+    Parameters
+    ==========
+
+    city : NetworkX Graph
+        The city.
+
+    route : tuple of ints
+        A tuple of indices of vertices which are connected by the route.
+
+    '''
+    route_up = city[route[0]][route[1]]
+    route_down = city[route[1]][route[0]]
+    cars_up = route_up["car_density"] * route_up["length"]
+    cars_down = route_down["car_density"] * route_down["length"]
+    total_cars = cars_up + cars_down
+    route_up_percentage = cars_up / total_cars
+    fuzzy_value = fuzzify(route_up_percentage)
+    return fuzzy_value
