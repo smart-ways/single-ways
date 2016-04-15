c2ad4d7dc674e5621138fa35c01ea433cec94de7
Kshitij Saraogi <KshitijSaraogi@gmail.com>
2016-04-15 21:03:27 +0530

    Updated the classifier model

--- src/model.py
+++ src/model.py
@@ -2,14 +2,15 @@ from __future__ import division
 
 from sklearn.neural_network import MLPClassifier
 
-def NN_model(X):
+def NN_model(X, target):
 	'''A perceptron classifier for classifying whether a route should be made
 	one-way or not for a particular time-period. 
 	
 	Parameters
 	==========
-	X : int, array
+	X : int
 	    An integer column matrix    
+	
 	The default algorithm ‘adam’ works pretty well on relatively large datasets
 	(with thousands of training samples or more) in terms of both training time and validation score. For small datasets, however, ‘l-bfgs’ can converge faster and perform better.
 
@@ -20,6 +21,8 @@ def NN_model(X):
     tol : float, optional, default 1e-4
 	
 	'''
+	y = [0, 1]
 	clf = MLPClassifier(hidden_layer_sizes=(0, 0), activation='logistic', algorithm='l-bfgs',
 		                early_stopping=True)
-	return clf.predict(X)
+	clf.fit(X, y)
+    return cls.predict(y)
